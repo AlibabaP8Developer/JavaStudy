781aabb1ba1c893afe860b130cef05d25b79855a
lizhenghang <1135365441@qq.com>
2022-02-28 14:11:53 +0800

    stream流

---
+++ java8/src/main/java/com/xiaomi/stream/Person.java
@@ -0,0 +1,15 @@
+package com.xiaomi.stream;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private String name;
+    private String sex;
+}
