4778bfdad2bf6379cabd9d96af56b2c8ab80fa2e
lizhenghang <1135365441@qq.com>
2022-03-15 16:27:02 +0800

    springboot

--- springboot/src/main/java/com/alibaba/SpringbootApplication.java
+++ springboot/src/main/java/com/alibaba/SpringbootApplication.java
@@ -26,7 +26,8 @@ public class SpringbootApplication {
         MyConfig bean = run.getBean(MyConfig.class);
         System.out.println(bean);
 
-        // @Configuration(proxyBeanMethods = true)代理对象调用方法，springboot总会检查这个组件是否在容器中存在
+        // @Configuration(proxyBeanMethods = true)代理对象调用方法，
+        // springboot总会检查这个组件是否在容器中存在
         // 保持组件单实例
         User user = bean.user01();
         User user1 = bean.user01();
