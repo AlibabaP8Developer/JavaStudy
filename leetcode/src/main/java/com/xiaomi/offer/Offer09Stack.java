7c5ea7a7b05dcac75471bd0c4420a64ee63b18bc
lizhenghang <1135365441@qq.com>
2022-03-17 16:01:06 +0800

    springboot

--- leetcode/src/main/java/com/xiaomi/offer/Offer09Stack.java
+++ leetcode/src/main/java/com/xiaomi/offer/Offer09Stack.java
@@ -1,5 +1,8 @@
 package com.xiaomi.offer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
  * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。
@@ -18,6 +21,7 @@ package com.xiaomi.offer;
 public class Offer09Stack {
 
     public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
 
     }
 }
