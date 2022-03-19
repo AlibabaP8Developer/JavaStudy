3ae83d48fd082b7a198e365caf686f7cd35a5e0b
lizhenghang <1135365441@qq.com>
2022-03-20 00:31:19 +0800

    面试

---
+++ common/src/main/java/com/alibaba/util/LocalDateUtil.java
@@ -0,0 +1,12 @@
+package com.alibaba.util;
+
+/**
+ * LocalDate
+ */
+public class LocalDateUtil {
+    /** 时间格式(yyyy-MM-dd) */
+    public final static String DATE_PATTERN = "yyyy-MM-dd";
+    /** 时间格式(yyyy-MM-dd HH:mm:ss) */
+    public final static String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+}
