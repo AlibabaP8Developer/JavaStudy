9d4df9643369bf301c48c06f6d7aa34132d92dd7
lizhenghang <1135365441@qq.com>
2022-03-20 12:50:31 +0800

    微服务

--- common/src/main/java/com/alibaba/util/LocalDateUtil.java
+++ common/src/main/java/com/alibaba/util/LocalDateUtil.java
@@ -1,12 +1,52 @@
 package com.alibaba.util;
 
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.LocalDate;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 /**
  * LocalDate
  */
 public class LocalDateUtil {
-    /** 时间格式(yyyy-MM-dd) */
+    /**
+     * 时间格式(yyyy-MM-dd)
+     */
     public final static String DATE_PATTERN = "yyyy-MM-dd";
-    /** 时间格式(yyyy-MM-dd HH:mm:ss) */
+    /**
+     * 时间格式(yyyy-MM-dd HH:mm:ss)
+     */
     public final static String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
+    /**
+     * 获取当前日期
+     *
+     * @return
+     */
+    public static LocalDateTime getNowDate() {
+        return LocalDateTime.now();
+    }
+
+    /**
+     * 格式化日期
+     *
+     * @param dateTime 日期
+     * @param format   格式化
+     * @return 返回值 String
+     */
+    public static String getFormat(LocalDateTime dateTime, String format) {
+        if (dateTime != null) {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern(format);
+            return dateTime.format(dtf);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime nowDate = LocalDateUtil.getNowDate();
+        String format = LocalDateUtil.getFormat(nowDate, "yyyy-MM-dd HH:mm:ss");
+        System.out.println(format);
+    }
 }
