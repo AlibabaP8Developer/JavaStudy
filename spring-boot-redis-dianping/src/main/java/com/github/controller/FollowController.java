a95045c21de4b15e1ad816ef8a6c5f3d48f6ff4e
lizhenghang <1135365441@qq.com>
2022-05-08 21:15:40 +0800

    redis项目案例:点赞、关注功能实现

--- spring-boot-redis-dianping/src/main/java/com/github/controller/FollowController.java
+++ spring-boot-redis-dianping/src/main/java/com/github/controller/FollowController.java
@@ -42,6 +42,11 @@ public class FollowController {
         return followService.isFollow(followUserId);
     }
 
+    /**
+     * 共同关注
+     * @param id
+     * @return
+     */
     @GetMapping("/common/{id}")
     public Result followCommons(@PathVariable("id") Long id) {
         return followService.followCommons(id);
