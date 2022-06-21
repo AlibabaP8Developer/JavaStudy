841a99cdd4eeed5664cb8ea109c0fb9af3775d57
admin <1135365441@qq.com>
2022-06-21 18:01:21 +0800

    sm2,sm3,sm4加密

---
+++ spring-boot-elasticsearch/src/main/java/com/github/controller/TestCBCController.java
@@ -0,0 +1,35 @@
+package com.github.controller;
+
+import com.github.util.CbcUtil;
+
+//@RestController
+//@RequestMapping("/test")
+public class TestCBCController {
+
+//    @GetMapping("")
+    public void testCbc(String sm4Key, String src, String iv) {
+        //String src = "I Love You";
+        //String iv = CbcUtil.get32UUID();
+        //String sm4Key = CbcUtil.generateSM4Key();
+        System.out.println("sm4Key:" + sm4Key);
+        System.out.println("--生成SM4结束--");
+        System.out.println("--SM4的CBC加密--");
+        String s1 = CbcUtil.SM4EncForCBC(sm4Key, src, iv);
+        System.out.println("密文:" + s1);
+        String text = CbcUtil.SM4DecForCBC(sm4Key, s1, iv);
+        System.out.println("CBC解密:   " + text);
+    }
+
+    public static void main(String[] args) {
+        String src = "I Love You";
+        String iv = CbcUtil.get32UUID();
+        String sm4Key = CbcUtil.generateSM4Key();
+        System.out.println("sm4Key:" + sm4Key);
+        System.out.println("--生成SM4结束--");
+        System.out.println("--SM4的CBC加密--");
+        String s1 = CbcUtil.SM4EncForCBC(sm4Key, src, iv);
+        System.out.println("密文:" + s1);
+        String text = CbcUtil.SM4DecForCBC(sm4Key, s1, iv);
+        System.out.println("CBC解密:   " + text);
+    }
+}
