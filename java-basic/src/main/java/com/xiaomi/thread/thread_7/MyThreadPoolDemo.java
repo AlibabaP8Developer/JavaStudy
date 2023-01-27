4e6bfdb515fc7bc38e63b68482097c7c3376b343
Edwin <1135365441@qq.com>
2023-01-27 21:03:01 +0800

    juc

--- java-basic/src/main/java/com/xiaomi/thread/thread_7/MyThreadPoolDemo.java
+++ java-basic/src/main/java/com/xiaomi/thread/thread_7/MyThreadPoolDemo.java
@@ -7,6 +7,7 @@ import java.util.concurrent.*;
 public class MyThreadPoolDemo {
     public static void main(String[] args) {
         /*
+            自定义线程池
             ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor();
             （核心线程数量，最大线程数量，空闲线程最大存活时间，任务队列，创建线程工厂，任务的拒绝策略）
             参数1：核心线程数量    不能小于0
@@ -16,6 +17,15 @@ public class MyThreadPoolDemo {
             参数5：任务队列       不能为null
             参数6：创建线程工厂    不能为null
             参数7：任务的拒绝策略  不能为null
+
+            线程池多大合适
+            CPU密集型运算=最大并行数+1
+            I/O密集型运算=最大并行数 * 期望CPU利用率 * (总时间（CPU计算时间+等待时间）/ CPU计算时间)
+
+            ThreadPoolExecutor.AbortPolicy 默认策略：丢弃任务并抛出RejectedExecutionException异常
+            ThreadPoolExecutor.DiscardPolicy 丢失任务，但是不抛出异常 这是不推荐的做法
+            ThreadPoolExecutor.DiscardOldestPolicy 抛弃队列中等待最久的任务，然后把当前任务加入到队列中
+            ThreadPoolExecutor.CallerRunsPolicy 调度任务的run()方法绕过线程池直接执行
          */
         ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(
                 3,  // 核心线程数量，能小于0
@@ -27,8 +37,8 @@ public class MyThreadPoolDemo {
                 new ThreadPoolExecutor.AbortPolicy() // 任务的拒绝策略
         );
 
-        ExecutorService pool = Executors.newFixedThreadPool(3);
-        pool.submit(new MyRunnable());
-        pool.shutdown();
+        poolExecutor.submit(new MyRunnable());
+
+        poolExecutor.shutdown();
     }
 }
