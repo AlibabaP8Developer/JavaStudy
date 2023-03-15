a44534acd83c3daf7bdb8443bfd4d671383f8ab6
Edwin <1135365441@qq.com>
2023-03-15 18:46:53 +0800

    删除

--- ruoyi-auth-parent/src/main/java/com/ruoyi/system/mapper/SysRoleMenuMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.system.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ruoyi.pojo.system.SysRoleMenu;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SysRoleMenuMapper extends BaseMapper<SysRoleMenu> {
-}
