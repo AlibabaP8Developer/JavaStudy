905dccf608ab0be001e97620a773a015190788a8
Edwin <1135365441@qq.com>
2023-10-12 21:09:55 +0800

    算法

---
+++ scala/src/main/java/com/itheima/interview/SinglyLinkedList.java
@@ -0,0 +1,29 @@
+package com.itheima.interview;
+
+/**
+ * 单向链表
+ */
+public class SinglyLinkedList {
+
+    Node head; // head头指针
+
+    /**
+     * 节点类
+     */
+    private static class Node {
+        int value; // 值
+        Node next;// 下一个节点指针
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public void addFirst() {
+        // 1.链表为空
+
+        // 2.链表非空
+    }
+}
+
