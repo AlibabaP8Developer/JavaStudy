88b8f0731b24f17319a55fafee2a7e3cd45e1d93
Edwin <1135365441@qq.com>
2023-11-05 12:17:11 +0800

    算法、Java17

--- scala/src/main/java/com/itheima/interview/SinglyLinkedList.java
+++ scala/src/main/java/com/itheima/interview/SinglyLinkedList.java
@@ -1,11 +1,47 @@
 package com.itheima.interview;
 
+import java.util.Iterator;
+import java.util.function.Consumer;
+
 /**
  * 单向链表
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList implements Iterable<Integer> {
 
     private Node head = null; // head头指针
+
+    /**
+     * 第三种遍历方式：实现Iterable<Integer>
+     *
+     * @return
+     */
+    @Override
+    public Iterator<Integer> iterator() {
+        // 匿名内部类 -> 带名字的内部类
+        return new Iterator<Integer>() {
+            Node p = head;
+
+            /**
+             * 询问是否有下一个元素
+             * @return
+             */
+            @Override
+            public boolean hasNext() {
+                return p != null;
+            }
+
+            /**
+             * 返回当前元素的值，并指向下一个元素
+             * @return
+             */
+            @Override
+            public Integer next() {
+                int v = p.value;
+                p = p.next;
+                return v;
+            }
+        };
+    }
 
     /**
      * 节点类
@@ -22,16 +58,36 @@ public class SinglyLinkedList {
 
     public void addFirst(int value) {
         // 1.链表为空
-        head = new Node(value, null);
+        //head = new Node(value, null);
         // 2.链表非空
         head = new Node(value, head);
     }
 
+    /**
+     * 遍历链表
+     */
     public void loop() {
         Node p = head;
         while (p != null) {
             System.out.println(p.value);
             p = p.next;
+        }
+    }
+
+    public void loop1(Consumer<Integer> consumer) {
+        Node p = head;
+        while (p != null) {
+            consumer.accept(p.value);
+            p = p.next;
+        }
+    }
+
+    /**
+     * 遍历链表2
+     */
+    public void loop2(Consumer<Integer> consumer) {
+        for (Node p = head; p != null; p = p.next) {
+            consumer.accept(p.value);
         }
     }
 
@@ -41,7 +97,12 @@ public class SinglyLinkedList {
         list.addFirst(2);
         list.addFirst(3);
         list.addFirst(4);
-        list.loop();
+        //list.loop2(System.out::println);
+
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+
     }
 }
 
